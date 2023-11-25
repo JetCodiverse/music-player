90a3b116c047f4c29e4cfaa993f6ee8753b1eb75
Jeter Trajano <jetertrajano2018@gmail.com>
2023-11-25 14:31:34 +0800

    first commit

---
+++ client/src/layouts/AuthLayout.jsx
@@ -0,0 +1,44 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const AuthLayout = () => {
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    }
+  }, [user]);
+  return (
+    <main>
+      <Flex
+        align="center"
+        justify="flex-start"
+        bg="zinc.800"
+        p={4}
+        pl={6}
+        h={{ base: "full", md: "5rem" }}
+      >
+        <Flex align="center" color="accent.main" justify="flex-start" gap={2}>
+          <img src="/soundbarIcon.svg" color="accent.main" />
+
+          <Heading
+            fontWeight="semibold"
+            color="gray.200"
+            fontSize={{ base: "lg", md: "2xl" }}
+          >
+            SoundBar.io
+          </Heading>
+        </Flex>
+      </Flex>
+      <Box bg="zinc.950" h="full" minH="91vh">
+        <Outlet />
+      </Box>
+    </main>
+  );
+};
+
+export default AuthLayout;
